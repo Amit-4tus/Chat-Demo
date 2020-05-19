648c94aee33cca02a25a3d56abe477fa42bc0468
Amit Fortus <amit.fortus4@gmail.com>
2020-05-20 00:43:22 +0300

    ready to deploy

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  });
+});
+
+http.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
